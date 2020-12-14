c3229f903a65f353dcb3ee102f4575679d219906
yaetiya <yaetiya@gmail.com>
2020-12-14 14:00:00 +0300

    to check

--- src/services/api/searchChannels.ts
+++ src/services/api/searchChannels.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { Channel } from "../../store/ducks/searchChannel/typescript/state";
 
 export const searchChannelsAPI = {
+  
   fetchChannels(payload: string): Promise<Channel[]> {
     return axios
       .get(`/users/search/${payload}`)
